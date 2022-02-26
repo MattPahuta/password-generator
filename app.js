06a39188b12c598133639ac4aaa90f439f865aae
Matt Haines <mhpahuta@gmail.com>
2022-02-26 14:27:39 -0800

    Add basic pw generation and display functionality

--- app.js
+++ app.js
@@ -8,4 +8,28 @@
     (use <input type="text"> to display the pw options)
 */
 
-const passwordChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "@", "$", "&", "%", "(", ")", ">", "<", "+", "-", "^", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const passwordChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "@", "$", "&", "%", "(", ")", ">", "<", "+", "-", "^"];
+
+const button = document.querySelector('button');
+const passwords = document.querySelectorAll('.password');
+
+function generatePasswords(length = 12) {
+  const newPW = [];
+
+  for (let i = 0; i < length; i += 1) {
+    const randomChar = passwordChars[Math.floor(Math.random() * passwordChars.length)];
+    newPW.push(randomChar)
+  }
+  return newPW.join('').toString();
+
+}
+
+function renderPasswords() {
+  passwords.forEach(function (pw) {
+    pw.textContent = generatePasswords();
+    pw.classList.add('accent');
+  });
+}
+
+
+button.addEventListener('click', renderPasswords);
