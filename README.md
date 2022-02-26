a35f8be240e088cf60129fb72a60c0e8a6344290
Matt Haines <mhpahuta@gmail.com>
2022-02-26 11:46:49 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Password Generator - Scrimba FEWD Project
+
+This is a solution to the Password Generator solo project, as part of the Scrimba Frontend Developer Career Path. 
+
+## Overview
+
+### Project Requirements 
+
+- Use an array to hold all possible password characters
+- Button to generate 4 random pw options
+- Display pw options
+- Stretch: Ability to set pw length 
+- Stretch: 1-click copy pw to user's clipboard
+
+### Built with 
+
+- Semantic HTML5 markup
+- Flexbox
+- Vanilla JavaScript
---
+++ app.js
@@ -0,0 +1,11 @@
+/* 
+  Requirements: 
+  - Use an array to hold all possible password characters
+  - Button to generate 4 random pw options
+  - Display pw options
+  - Stretch: Ability to set pw length 
+  - Stretch: 1-click copy pw to user's clipboard
+    (use <input type="text"> to display the pw options)
+*/
+
+const passwordChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "@", "$", "&", "%", "(", ")", ">", "<", "+", "-", "^", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
