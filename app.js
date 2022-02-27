5682c23fae56dea9875a1340c2d3573982834f02
Matt Haines <mhpahuta@gmail.com>
2022-02-27 14:52:42 -0800

    Add pw length selection input, functionality

--- app.js
+++ app.js
@@ -10,10 +10,14 @@
 
 const passwordChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "@", "$", "&", "%", "(", ")", ">", "<", "+", "-", "^"];
 
+const pwLength = document.querySelector('#pw-length');
 const button = document.querySelector('button');
 const passwords = document.querySelectorAll('.password');
+const pwContainers = document.querySelectorAll('.password-container');
 
-function generatePasswords(length = 12) {
+const testPW = document.querySelector('.test-pw');
+
+function generatePasswords(length) {
   const newPW = [];
 
   for (let i = 0; i < length; i += 1) {
@@ -25,11 +29,25 @@ function generatePasswords(length = 12) {
 }
 
 function renderPasswords() {
+  const length = Number(pwLength.value);
+
+  if (length < 8 || length > 30) {
+    alert('Enter a number between 8 and 30');
+    pwLength.value = '';
+    return;
+  }
+
   passwords.forEach(function (pw) {
-    pw.textContent = generatePasswords();
+    pw.textContent = generatePasswords(length);
     pw.classList.add('accent');
+    pwLength.value = '';
   });
+
 }
 
 
+
+
+
 button.addEventListener('click', renderPasswords);
+// button.addEventListener('click', renderPWInputs);
