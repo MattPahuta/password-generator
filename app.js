c02703a5828b0cca6ec7bd211dcecde5507e1a27
Matt Haines <mhpahuta@gmail.com>
2022-02-28 11:01:03 -0800

    Add copy pw ability, style and markup cleanup

--- app.js
+++ app.js
@@ -5,17 +5,15 @@
   - Display pw options
   - Stretch: Ability to set pw length 
   - Stretch: 1-click copy pw to user's clipboard
-    (use <input type="text"> to display the pw options)
 */
 
-const passwordChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "@", "$", "&", "%", "(", ")", ">", "<", "+", "-", "^"];
+const passwordChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "@", "$", "&", "%", "(", ")", ">", "<", "+", "-", "^", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 const pwLength = document.querySelector('#pw-length');
 const button = document.querySelector('button');
 const passwords = document.querySelectorAll('.password');
 const pwContainers = document.querySelectorAll('.password-container');
-
-const testPW = document.querySelector('.test-pw');
+const pwMessage = document.querySelector('.pw-message');
 
 function generatePasswords(length) {
   const newPW = [];
@@ -28,7 +26,7 @@ function generatePasswords(length) {
 
 }
 
-function renderPasswords() {
+function renderPasswords () {
   const length = Number(pwLength.value);
 
   if (length < 8 || length > 30) {
@@ -40,14 +38,34 @@ function renderPasswords() {
   passwords.forEach(function (pw) {
     pw.textContent = generatePasswords(length);
     pw.classList.add('accent');
+    pw.classList.add('generated-pw');
     pwLength.value = '';
   });
 
+  addClickToCopy();
+  pwMessage.removeAttribute('hidden');
+
 }
 
+// function copyToClipboard() {
+//   var copyText = document.getElementById("content").value;
+//   navigator.clipboard.writeText(copyText).then(() => {
+//       alert("Copied to clipboard");
+//   });
+// }
 
-
-
+function addClickToCopy() {
+// const addCopy = () => {
+  passwords.forEach(function(pw) {
+    pw.addEventListener('click', function(e) {
+      // console.log(e.target.textContent);
+      let copyText = e.target.textContent;
+  
+      navigator.clipboard.writeText(copyText).then(() => {
+        alert('copied to clipboard')
+      })
+    });
+  })
+}
 
 button.addEventListener('click', renderPasswords);
-// button.addEventListener('click', renderPWInputs);
